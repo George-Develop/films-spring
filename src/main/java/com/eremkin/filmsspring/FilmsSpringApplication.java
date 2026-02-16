e91994df6297d410a96ef3b2cd5a942fb72d255e
MRowa <49066157+RowaEremkin@users.noreply.github.com>
2026-02-17 01:10:17 +0300

    Lab 2.1

---
+++ src/main/java/com/eremkin/filmsspring/FilmsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.eremkin.filmsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmsSpringApplication.class, args);
+    }
+
+}
